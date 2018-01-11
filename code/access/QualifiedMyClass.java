6f5b3be94cd00a5f3db9572f7cc096485c7211ff
ylhao <765422195@qq.com>
2018-01-11 13:08:44 +0800

    第六章

--- code/access/QualifiedMyClass.java
+++ code/access/QualifiedMyClass.java
@@ -1,8 +1,9 @@
 //: access/QualifiedMyClass.java
 
+// javac .\QualifiedMyClass.java .\mypackage\*.java
+// javac .\access\QualifiedMyClass.java .\access\mypackage\*.java
 public class QualifiedMyClass {
   public static void main(String[] args) {
-    access.mypackage.MyClass m =
-      new access.mypackage.MyClass();
+    access.mypackage.MyClass m = new access.mypackage.MyClass();
   }
 } ///:~
--- code/initialization/Sing1.java
+++ code/initialization/Sing1.java
@@ -1,4 +1,4 @@
 public class Sing1 {
-    int i = 0;  // 域 i 默认具有包访问权限
-    void sing() {}  // 方法 sing() 默认具有包访问权限
+    int i = 0;
+    void sing() {}
 }
